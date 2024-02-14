ae795bdf70cfaf0d962a3ff20779382b429db201
caylynmac <152448716+caylynmac@users.noreply.github.com>
2024-02-14 15:31:04 -0700

    Update Dishwasher.cs

--- Group5_Assignment1/Entities/Dishwasher.cs
+++ Group5_Assignment1/Entities/Dishwasher.cs
@@ -1,44 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Group5_Assignment1.Entities.Abstract;
-
 namespace Group5_Assignment1.Entities
 {
     internal class Dishwasher : Appliance
     {
-        public const string Quietest = "Qt";
-        public const string Quieter = "Qr";
-        public const string Quiet = "Q";
-        public const string Moderate = "M";
+        //sound rating constants
 
-        private readonly string _feature;
-        private readonly string _soundRating;
+        public const string SoundRatingModerate = "M";
+        public const string SoundRatingQuiet = "Qu";
+        public const string SoundRatingQuieter = "Qr";
+        public const string SoundRatingQuietest = "Qt";
 
-        public string Feature { get { return _feature; } }
-        public string SoundRating { get { return _soundRating; } }
+        //private fields
+        private string _feature;
+        private string _soundRating;
 
+        //properties
+        public string Feature
+        {
+            get { return _feature; }
+        }
+
+        public string SoundRating
+        {
+            get { return _soundRating; }
+        }
+
+        //returns a word for the display
         public string SoundRatingDisplay
         {
             get
             {
-                if (_soundRating == Quietest)
-                    return "Quietest";
-                else if (_soundRating == Quieter)
-                    return "Quieter";
-                else if (_soundRating == Quiet)
-                    return "Quiet";
-                else if (_soundRating == Moderate)
-                    return "Moderate";
-                else
-                    return "Unknown";
+                switch (_soundRating)
+                {
+                    case SoundRatingModerate:
+                        return "Moderate";
+                    case SoundRatingQuiet:
+                        return "Quiet";
+                    case SoundRatingQuieter:
+                        return "Quieter";
+                    case SoundRatingQuietest:
+                        return "Quietest";
+                    default:
+                        return "(Unknown)";
+                }
             }
         }
 
-        public Dishwasher(long itemNumber, string brand, int quantity, decimal wattage, string color, decimal price, string feature, string soundRating)
-            : base(itemNumber, brand, quantity, wattage, color, price)
+        //constructor
+        //passes arguments to base class constructor
+        public Dishwasher(long itemNumber, string brand, int quantity, decimal wattage, string color, decimal price, string feature, string soundRating) : base(itemNumber, brand, quantity, wattage, color, price)
         {
             this._feature = feature;
             this._soundRating = soundRating;
@@ -46,18 +62,25 @@ namespace Group5_Assignment1.Entities
 
         public override string FormatForFile()
         {
-            return $"{base.FormatForFile()};{_feature};{_soundRating}";
+            return string.Join(';', base.FormatForFile(), this._feature, this._soundRating); 
+            //concatenates additional dishwasher fields to common appliance fields formatted in appliance class
         }
-
         public override string ToString()
         {
-            return $"Item Number: {ItemNumber}\n" +
-                   $"Brand: {Brand}\n" +
-                   $"Quantity: {Quantity}\n" +
-                   $"Color: {Color}\n" +
-                   $"Price: {Price}\n" +
-                   $"Feature: {Feature}\n" +
-                   $"Sound Rating: {SoundRatingDisplay}\n";
+            string s =
+
+                $"Item Number: {ItemNumber}\n" +
+                $"Brand: {Brand}\n" +
+                $"Quantity: {Quantity}\n" +
+                $"Wattage: {Wattage}\n" +
+                $"Color: {Color}\n" +
+                $"Price: {Price}\n" +
+                $"Feature: {_feature}\n" +
+                $"SoundRating: {SoundRatingDisplay}\n";
+
+            return s;
+         
         }
+
     }
 }
