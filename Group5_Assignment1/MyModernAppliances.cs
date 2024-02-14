b42dc7882880f6118eda0ce9f127d812d19abf42
caylynmac <152448716+caylynmac@users.noreply.github.com>
2024-02-14 15:26:39 -0700

    Update MyModernAppliances.cs

--- Group5_Assignment1/MyModernAppliances.cs
+++ Group5_Assignment1/MyModernAppliances.cs
@@ -1,18 +1,29 @@
-﻿using System;
+using Group5_Assignment1.Entities.Abstract;
+using Group5_Assignment1.Entities;
+using System;
 using System.Collections.Generic;
-using ModernAppliances.Entities;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
-namespace ModernAppliances
+namespace Group5_Assignment1
 {
+
     internal class MyModernAppliances : ModernAppliances
     {
+        public MyModernAppliances() :base() { }
+
+        //option 1, check out appliance
         public override void Checkout()
         {
-            Console.Write("Enter the item number of an appliance: ");
+            //get item number from user
+            Console.Write("\nEnter the item number of an Appliance: \n");
             long itemNumber = long.Parse(Console.ReadLine());
 
             Appliance foundAppliance = null;
 
+            //iterate through list, find matching item number
             foreach (Appliance appliance in Appliances)
             {
                 if (appliance.ItemNumber == itemNumber)
@@ -22,31 +33,37 @@ namespace ModernAppliances
                 }
             }
 
+            //state if found and then check if available
+            //call checkout method from object to mark as unavailable
             if (foundAppliance == null)
             {
-                Console.WriteLine("No appliances found with that item number.");
+                Console.WriteLine("\nNo appliances found with that item number.\n");
             }
             else
             {
                 if (foundAppliance.IsAvailable)
                 {
                     foundAppliance.Checkout();
-                    Console.WriteLine("Appliance has been checked out.");
+                    Console.WriteLine($"\nAppliance '{foundAppliance.ItemNumber}' has been checked out.\n");
                 }
                 else
                 {
-                    Console.WriteLine("The appliance is not available to be checked out.");
+                    Console.WriteLine("\nThe appliance is not available to be checked out.\n");
                 }
             }
         }
 
+        //option 2, find appliances by brand
         public override void Find()
         {
-            Console.Write("Enter brand to search for: ");
+            //ask for brand name
+            Console.WriteLine("\nEnter brand to search for: \n");
             string brandToSearch = Console.ReadLine();
 
+            //list to store found appliances
             List<Appliance> foundAppliances = new List<Appliance>();
 
+            //iterate through list, add matching objects to found list
             foreach (Appliance appliance in Appliances)
             {
                 if (appliance.Brand == brandToSearch)
@@ -54,156 +71,150 @@ namespace ModernAppliances
                     foundAppliances.Add(appliance);
                 }
             }
-
+            Console.WriteLine("\nMatching Appliances:\n");
+            //call function to display list
             DisplayAppliancesFromList(foundAppliances);
         }
 
+        //option 3 - 1 display refridgerators
         public override void DisplayRefrigerators()
         {
-            Console.WriteLine("Possible options:");
-            Console.WriteLine("0 - Any");
-            Console.WriteLine("2 - Double doors");
-            Console.WriteLine("3 - Three doors");
-            Console.WriteLine("4 - Four doors");
-
-            Console.Write("Enter number of doors: ");
+            //get number of doors
+            Console.Write("\nEnter number of doors: 2 (double door), 3 (three doors) or 4 (four doors):\n");
             int numberOfDoors = int.Parse(Console.ReadLine());
 
+            //list for found refrigerator objects
             List<Appliance> foundRefrigerators = new List<Appliance>();
 
             foreach (Appliance appliance in Appliances)
             {
-                if (appliance is Refrigerator refrigerator && (numberOfDoors == 0 || refrigerator.NumberOfDoors == numberOfDoors))
+                if ((appliance.Type == "Refrigerator"))
                 {
-                    foundRefrigerators.Add(appliance);
+                    Refrigerator refrigerator = (Refrigerator)appliance; //cast appliance to refrigerator to access doors
+
+                    if (refrigerator.Doors == numberOfDoors)
+                    {
+                        foundRefrigerators.Add(refrigerator);
+                    }
                 }
             }
 
+            Console.WriteLine("\nMatching refrigerators:\n");
             DisplayAppliancesFromList(foundRefrigerators);
+
         }
 
+        //option 3 - 2 display vacuums
         public override void DisplayVacuums()
         {
-            Console.WriteLine("Possible options:");
-            Console.WriteLine("0 - Any");
-            Console.WriteLine("1 - Residential");
-            Console.WriteLine("2 - Commercial");
+            //display 
 
-            Console.Write("Enter grade: ");
-            int grade = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("Possible options:");
-            Console.WriteLine("0 - Any");
-            Console.WriteLine("1 - 18 Volt");
-            Console.WriteLine("2 - 24 Volt");
-
-            Console.Write("Enter voltage: ");
+            Console.Write("\nEnter battery voltage value. 18 V (low) or 24 V (high)\n");
             int voltage = int.Parse(Console.ReadLine());
 
             List<Appliance> foundVacuums = new List<Appliance>();
 
             foreach (Appliance appliance in Appliances)
             {
-                if (appliance is Vacuum vacuum &&
-                    (grade == 0 || (int)vacuum.Grade == grade) &&
-                    (voltage == 0 || vacuum.Voltage == voltage))
+                if ((appliance.Type == "Vacuum"))
                 {
-                    foundVacuums.Add(appliance);
+                    Vacuum vacuum = (Vacuum)appliance; //cast appliance to vacuum to access voltage
+
+                    if (vacuum.BatteryVoltage == voltage)
+                    {
+                        foundVacuums.Add(vacuum);
+                    }
                 }
             }
 
+            Console.WriteLine("\nMatching Vacuums:\n");
             DisplayAppliancesFromList(foundVacuums);
         }
 
+        //option 3- 3 display microwaves
         public override void DisplayMicrowaves()
         {
-            Console.WriteLine("Possible options:");
-            Console.WriteLine("0 - Any");
-            Console.WriteLine("1 - Kitchen");
-            Console.WriteLine("2 - Work site");
+            //ask for room type
 
-            Console.Write("Enter room type: ");
+            Console.Write("\nRoom where the microwave will be installed: K (kitchen) or W (work site): \n");
             char roomType = char.ToUpper(Console.ReadLine()[0]);
 
+            //empty list for found microwaves
             List<Appliance> foundMicrowaves = new List<Appliance>();
 
             foreach (Appliance appliance in Appliances)
             {
-                if (appliance is Microwave microwave && (roomType == 'A' || microwave.RoomType == roomType))
+                if ((appliance.Type == "Microwave"))
                 {
-                    foundMicrowaves.Add(appliance);
+                    Microwave microwave = (Microwave)appliance; 
+
+                    if (microwave.RoomType == roomType)
+                    {
+                        foundMicrowaves.Add(microwave);
+                    }
                 }
             }
-
+            Console.WriteLine("\nMatching microwaves:\n");
             DisplayAppliancesFromList(foundMicrowaves);
         }
 
+        //option 3 - 4 display dishwashers
         public override void DisplayDishwashers()
         {
-            Console.WriteLine("Possible options:");
-            Console.WriteLine("0 - Any");
-            Console.WriteLine("1 - Quietest");
-            Console.WriteLine("2 - Quieter");
-            Console.WriteLine("3 - Quiet");
-            Console.WriteLine("4 - Moderate");
 
-            Console.Write("Enter sound rating: ");
+            //ask for sound rating
+
+            Console.Write("\nEnter the sound rating of the dishwasher: Qt (Quietest), Qr (Quieter), Qu(Quiet) or M (Moderate):\n");
             string soundRating = Console.ReadLine();
 
+            //empty list for found dishwasher
             List<Appliance> foundDishwashers = new List<Appliance>();
 
+            //iterate through appliance list, find matches
             foreach (Appliance appliance in Appliances)
             {
-                if (appliance is Dishwasher dishwasher && (soundRating == "0" || dishwasher.SoundRating == soundRating))
+                if ((appliance.Type == "Dishwasher"))
                 {
-                    foundDishwashers.Add(appliance);
+                    Dishwasher dishwasher = (Dishwasher)appliance; //cast appliance to vacuum to access voltage
+
+                    if (dishwasher.SoundRating == soundRating)
+                    {
+                        Console.WriteLine(dishwasher);
+                        foundDishwashers.Add(dishwasher);
+                    }
                 }
             }
-
+            Console.WriteLine("\nMatching Dishwashers:\n");
             DisplayAppliancesFromList(foundDishwashers);
         }
 
+        //option 4
         public override void RandomList()
         {
-            Console.WriteLine("Appliance Types");
-            Console.WriteLine("0 - Any");
-            Console.WriteLine("1 – Refrigerators");
-            Console.WriteLine("2 – Vacuums");
-            Console.WriteLine("3 – Microwaves");
-            Console.WriteLine("4 – Dishwashers");
-
-            Console.Write("Enter type of appliance: ");
-            int applianceType = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter number of appliances: ");
+            //get number of appliances for list
+            Console.Write("\nEnter number of appliances: \n");
             int numberOfAppliances = int.Parse(Console.ReadLine());
 
+            //length of Appliances list
+            int length = Appliances.Count;
+
+            int randIndex;
+            var rand = new Random();
+
+            //empty list for found appliances
             List<Appliance> foundAppliances = new List<Appliance>();
 
-            foreach (Appliance appliance in Appliances)
+            //generate random numbers between 0 and length of appliances up to user requested number of appliances
+            for (int i = 0; i <= (numberOfAppliances-1); i++)
             {
-                if ((applianceType == 0 || (applianceType == 1 && appliance is Refrigerator) ||
-                    (applianceType == 2 && appliance is Vacuum) || (applianceType == 3 && appliance is Microwave) ||
-                    (applianceType == 4 && appliance is Dishwasher)))
-                {
-                    foundAppliances.Add(appliance);
-                }
+                randIndex = rand.Next((length));
+
+                //add indexed appliance to new random list
+                foundAppliances.Add(Appliances[randIndex]);
             }
 
-            foundAppliances.Sort(new RandomComparer());
-            DisplayAppliancesFromList(foundAppliances, numberOfAppliances);
-        }
-
-        private void DisplayAppliancesFromList(List<Appliance> appliances, int maxItems = 0)
-        {
-            int count = 0;
-            foreach (var appliance in appliances)
-            {
-                Console.WriteLine(appliance);
-                count++;
-                if (maxItems != 0 && count == maxItems)
-                    break;
-            }
+            Console.WriteLine("\nRandom appliances:\n");
+            DisplayAppliancesFromList(foundAppliances);
         }
     }
 }
